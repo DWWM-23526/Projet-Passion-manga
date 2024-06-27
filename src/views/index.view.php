5ddc7909e739f3c69addc6234e9b772b49fddb01
AllFatherOdin0 <stepien.f.clement@gmail.com>
2024-06-27 11:26:55 +0200

    basic partials

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -1,1 +1,11 @@
-<h1><?= $titlePage ?></h1>
+<?php
+
+require __DIR__ . '/../partials/head.php';
+?>
+<main>
+    <h1><?= $titlePage?> </h1>
+</main>
+<?php
+require __DIR__ . '/../partials/footer.php';
+require __DIR__ . '/../partials/foot.php';
+?>

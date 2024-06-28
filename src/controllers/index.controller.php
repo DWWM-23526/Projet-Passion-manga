2f277b72fedf2f87506729e7440e210bd5f1f1b3
Pierre-Marie <pierremariemarchio@gmail.com>
2024-06-28 11:43:10 +0200

    start database class implementation

--- src/controllers/index.controller.php
+++ src/controllers/index.controller.php
@@ -1,7 +1,15 @@
 <?php
 
+require __DIR__ . '/../core/Database.php';
+$config = require __DIR__ . '/../config.php';
+$db = Database::getInstance($config['database']);
+
+$mangas = $db->query("SELECT * FROM mangas");
+
+var_dump($mangas);
 
 $titlePage = 'coucouc from index';
+
 
 
 require __DIR__ . '/../views/index.view.php';
--- src/core/Database.php
+++ src/core/Database.php
@@ -1,1 +1,56 @@
 <?php
+
+
+class Database
+{
+    private static $instance = null;
+    private $connexion;
+    private $statement;
+ 
+
+    private function __construct($config)
+    {
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['db']};charset={$config['charset']}";
+
+        $option = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        
+        $this->connexion = $this->connexion($dsn, $config['user'], $config['pass'], $option);
+
+    }
+
+    public static function getInstance($config)
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($config);
+        }
+
+        return self::$instance;
+    }
+
+    public function query( $query, $params = []) {
+        $this->statement = $this->connexion->prepare($query);
+        $this->statement->execute($params);
+        return $this;
+
+    }
+
+
+    private function connexion($dsn, $user, $pass, $option)
+    {
+
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $option);
+
+        } catch (PDOException $e)  {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+
+        return $pdo;
+    }
+
+
+}

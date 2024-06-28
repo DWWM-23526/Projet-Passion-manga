aaf6349f4cd78a34fe5e2898cc4da30c922168fa
Pierre-Marie <pierremariemarchio@gmail.com>
2024-06-28 23:55:58 +0200

    chores

--- src/partials/head.php
+++ src/partials/head.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Passion Manga</title>
 
-    <link rel="stylesheet" href="/assets/styles/styles.css">
+    <link rel="stylesheet" href="/assets/styles/main.css">
     <link rel="stylesheet" href="/assets/styles/partials/navBar.css">
     <link rel="stylesheet" href="/assets/styles/partials/searchBar.css">
     <link rel="stylesheet" href="/assets/styles/partials/card.css">

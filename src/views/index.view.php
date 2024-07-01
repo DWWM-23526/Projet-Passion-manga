a5da0972e21034ccce453840d4e71e4d54f20296
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-07-01 09:16:50 +0200

    fix h2

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -7,25 +7,25 @@ require_once __DIR__ . '/../partials/header.php';
 <main class="container">
 
     <section id="mangas" class="my-5">
-        <a href="/mangas">
-            <h2>MANGAS</h2>
-        </a>
+        
+            <h2><a href="/mangas">MANGAS</a></h2>
+        
 
         <p>Explorez notre vaste bibliothèque de mangas. De l'action trépidante aux romances émouvantes, en passant par des thrillers palpitants et des comédies hilarantes, notre collection couvre tous les genres pour satisfaire tous les goûts.</p>
     </section>
 
     <section id="mangakas" class="my-5">
-        <a href="/mangakas">
-            <h2>MANGAKAS</h2>
-        </a>
+        
+            <h2><a href="/mangakas">MANGAKAS</a></h2>
+        
 
         <p>Découvrez les artistes derrière vos mangas préférés. Notre section Mangakas vous présente les auteurs et illustrateurs, leur biographie, leurs œuvres majeures et leurs inspirations.</p>
     </section>
 
     <section id="genres" class="my-5">
-        <a href="/genres">
-            <h2>Genres</h2>
-        </a>
+        
+            <h2><a href="/genres">GENRES</a></h2>
+        
 
         <p>Trouvez les mangas qui correspondent exactement à vos goûts en parcourant notre section Genres. Que vous soyez fan de shōnen, shōjo, seinen, josei, ou encore des sous-genres comme le mecha, l'isekai, ou le slice of life, nous avons organisé notre bibliothèque pour faciliter votre recherche.</p>
     </section>

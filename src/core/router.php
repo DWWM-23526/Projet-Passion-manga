6c175b58b0b654bc4b07c042ea2a9448a8b2a951
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-02 14:09:58 +0200

    start routing

--- src/core/router.php
+++ src/core/router.php
@@ -1,27 +1,32 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-$uri = urldecode($uri);
+// $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+// $uri = urldecode($uri);
 
-$routes = require __DIR__ . '/../routes.php';
+// $routes = require __DIR__ . '/../routes.php';
 
 
-function abort($error = 404)
+// function abort($error = 404)
+// {
+//     http_response_code($error);
+//     require_once __DIR__ . '/../views/errors/404.php';
+//     die();
+// }
+
+
+// function routeToController($key, $array)
+// {
+
+//     if (array_key_exists($key, $array)) {
+//         require __DIR__ . '/../' . $array[$key];
+//     } else {
+//         abort();
+//     }
+// };
+
+// routeToController($uri, $routes);
+
+class Router 
 {
-    http_response_code($error);
-    require_once __DIR__ . '/../views/errors/404.php';
-    die();
+    
 }
-
-
-function routeToController($key, $array)
-{
-
-    if (array_key_exists($key, $array)) {
-        require __DIR__ . '/../' . $array[$key];
-    } else {
-        abort();
-    }
-};
-
-routeToController($uri, $routes);
--- src/routes.php
+++ src/routes.php
@@ -1,18 +1,19 @@
 <?php
 
+// return [
 
-return [
+//     '/' =>  'controllers/index.controller.php',
+//     '/login' =>  'controllers/login/login.controller.php',
+//     '/register' => 'controllers/login/register.controller.php',
 
-    '/' =>  'controllers/index.controller.php',
-    '/login' =>  'controllers/login/login.controller.php',
-    '/register' => 'controllers/login/register.controller.php',
+//     '/mangas' => 'controllers/mangas/index.mangas.controller.php',
+//     '/mangas/manga' => 'controllers/mangas/show.mangas.controller.php',
 
-    '/mangas' => 'controllers/mangas/index.mangas.controller.php',
-    '/mangas/manga' => 'controllers/mangas/show.mangas.controller.php',
+//     '/mangakas' => 'controllers/mangakas/index.mangakas.controller.php',
+//     '/mangakas/mangaka' => 'controllers/mangakas/show.mangakas.controller.php',
 
-    '/mangakas' => 'controllers/mangakas/index.mangakas.controller.php',
-    '/mangakas/mangaka' => 'controllers/mangakas/show.mangakas.controller.php',
+//     '/genres' => 'controllers/tags/index.tags.controller.php'
 
-    '/genres' => 'controllers/tags/index.tags.controller.php'
+// ];
 
-];
+

3adbaaf003ec3bb445168dead61da497359a401b
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-03 16:12:19 +0200

    hot fix

--- src/controllers/mangas/show.mangas.controller.php
+++ src/controllers/mangas/show.mangas.controller.php
@@ -3,15 +3,8 @@
 use core\App;
 use services\MangasService;
 
-
-
-
-// $manga = $db->query("SELECT * FROM mangas where Id_manga = :id", ['id' => $_GET['id']])->fetchOrFail();
-
 $db = App::getServicesContainer()->getContainer(MangasService::class);
 $manga = $db->selectById($_GET['id']);
-
-var_dump($manga);
 
 $headerTitle = $manga['manga_name'];
 require __DIR__ . '/../../views/mangas/show.mangas.view.php';

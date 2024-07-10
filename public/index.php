35dd7e8485859383c7f074b13e650c6edb5158f7
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-10 14:19:32 +0200

    token implementation

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ session_start();
 
 // AUTOLOAD CLASS
 
+require __DIR__ . '/../vendor/autoload.php';
+
 spl_autoload_register(function ($class){
     $result = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     require __DIR__ . "/../src/{$result}.php";

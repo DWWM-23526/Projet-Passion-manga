6b75e1168d22a4cf912d58b9dc879a4d6dfa7575
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-10 08:13:11 +0200

    hotfix

--- src/middlewares/GuestMiddleware.php
+++ src/middlewares/GuestMiddleware.php
@@ -6,7 +6,10 @@ class GuestMiddleware extends Middleware
 {
     public function handle()
     {
+        
         if ($_SESSION['user'] ?? false) {
+            
+            require_once __DIR__ . '/../utils/urlis.php';
 
             if (urlIs('/login')) {
                 $this->redirect('login');
